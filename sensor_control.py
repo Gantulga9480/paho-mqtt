66eb4ee4447e3e793dc4b8a9ddf49827ce3ab665
Gantulga <limited.tulgaa@gmail.com>
2021-01-23 03:27:59 +0800

    fix new

--- sensor_control.py
+++ sensor_control.py
@@ -305,8 +305,8 @@ class SensorControl(Tk):
                            self.activity_time_list[1][index], 1):
                 act_frame.append(self.sensor_stream[i])
             data_time = self.real_time_start[index].strftime(DATE_TIME)
-            os.makedirs(f"/activity/{label}/{data_time}/")
-            path = f"/activity/{label}/{data_time}/{data_time}"
+            os.makedirs(f"activity/{label}/{data_time}", mode="")
+            path = f"activity/{label}/{data_time}/{data_time}"
             data_open = open(f"{path}.csv", "w+", newline='')
             writer = csv.writer(data_open)
             with data_open:

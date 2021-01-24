55cc374f526a267289cb9c030fd69dcb93a7af0c
Gantulga <limited.tulgaa@gmail.com>
2021-01-25 00:20:30 +0800

    add

--- app/data_stream.py
+++ app/data_stream.py
@@ -5,9 +5,8 @@ from app.utils import BUFFER_THRESHOLD
 
 class Stream:
 
-    def __init__(self, sensor=None, kinect=None):
+    def __init__(self, sensor=None):
         self.sensor = sensor
-        self.kinect = kinect
         self.sensor_ignore = False
         self.buffer_ignore = False
         self.sensor_buffer_empty_count = 0
@@ -39,41 +38,6 @@ class Stream:
                     print(f"Not connected to {sensor.info}")
                     raise BufferError
         return data
-
-    def get_video_stream(self):
-        video = list()
-        depth = list()
-        for index, kinect in enumerate(self.kinect):
-            rgb_len = len(kinect.rgb_buffer)
-            dp_len = len(kinect.depth_buffer)
-            a_dp_len = len(kinect.azure_rgb_buffer)
-            a_rgb_len = len(kinect.azure_depth_buffer)
-            if rgb_len > 2 and dp_len > 2 and a_dp_len > 2 and a_rgb_len > 2:
-                dp = kinect.depth_buffer.pop(0)
-                vd = kinect.rgb_buffer.pop(0)
-                a_vd = kinect.azure_rgb_buffer.pop(0)
-                a_dp = kinect.azure_depth_buffer.pop(0)
-
-                video.append(vd)
-                depth.append(dp)
-                video.append(a_vd)
-                depth.append(a_dp)
-                if rgb_len > BUFFER_THRESHOLD or dp_len > BUFFER_THRESHOLD:
-                    if self.buffer_ignore:
-                        pass
-                    else:
-                        print("Kinect data overflow error on", index)
-                        print(f"rgb: {rgb_len}, depth: {dp_len}")
-                        print(f'a_rgb: {a_rgb_len}, a_depth: {a_dp_len}')
-                        raise BufferError
-            else:
-                print(f"Kinect-{index+1} data buffer is currently empty")
-                self.video_buffer_empty_count += 1
-                if self.video_buffer_empty_count > BUFFER_EMPTY_THRESHOLD:
-                    print(f"Not connected to {kinect.id_name}:{kinect.type}")
-                    raise BufferError
-                return False, False
-        return video, depth
 
     def set_error(self, s_ignore, b_ignore):
         self.sensor_ignore = s_ignore
--- azure.py
+++ azure.py
@@ -1,4 +1,5 @@
 from pyk4a import PyK4A
+from freenect import sync_get_depth as get_depth, sync_get_video as get_video
 import cv2
 import numpy as np
 import rospy
@@ -7,16 +8,21 @@ from cv_bridge import CvBridge
 
 
 def main():
+    (depth, _), (rgb, _) = get_depth(), get_video()
     k4a = PyK4A()
     k4a.start()
     print("azure kinect ready")
     while True:
+        (depth, _), (rgb, _) = get_depth(), get_video()
         capture = k4a.get_capture()
         img_color = capture.color
         img_depth = capture.depth
+        cv2.normalize(depth, depth, 0, 255, cv2.NORM_MINMAX)
+        depth = np.round(depth).astype(np.uint8)
         cv2.normalize(img_depth, img_depth, 0, 255, cv2.NORM_MINMAX)
         img_color = np.round(img_color).astype(np.uint8)
         img_depth = np.round(img_depth).astype(np.uint8)
+        cv2.imshow("kinect_1", depth)
         cv2.imshow("azure_1", img_depth)
         cv2.waitKey(5)
 
